b124921726625a954f8468c352971fc76e8bc765
yashdatir1999 <yashdatir1999@gmail.com>
2023-10-26 22:10:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// // var h=document.querySelector("h1")
+// // var b=document.querySelector("button")
+// // var img=document.querySelector(".box")
+
+// // b.addEventListener("click",function(){
+
+// //     img.style.backgroundImage="url(https://images.unsplash.com/photo-1624634213708-5b9782ad4b4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60)"
+// //     img.style.borderRadius="100vmax"
+    
+// // })
+
+
+// // b.addEventListener("click",function(){
+// //     h.innerHTML=("YASH")
+// //     h.style.color="red"
+// //     h.style.backgroundColor="black"
+// //     h.style.fontSize="50px"
+// //     h.style.margin="20px 50px"
+// //     b.style.backgroundColor=" rgba(0, 0, 0, 0.624)"
+// //     // b.style.background="linear-gradient(red, pink)"
+// //     b.style.border="2px solid red"
+// //     b.style.padding="5px"
+    
+    
+// // })
+
+
+// var light=document.querySelector(".model")
+// var dark=document.querySelector(".moded")
+// var page=document.querySelector(".page1")
+// var h1=document.querySelector("h1")
+// light.addEventListener("click",function(){
+//     page.style.backgroundColor="white"
+//     h1.innerHTML="GOOD MORNING"
+//     h1.style.color="black"
+//     h1.style.left="30%"
+//     dark.style.color="white"
+//     dark.style.backgroundColor="black"
+//     dark.style.fontWeight="900"
+//     light.style.color="black"
+//     light.style.backgroundColor="white"
+//     light.style.fontWeight="900"
+//     h1.style.fontFamily="cursive"
+//     light.style.boxShadow="0 0 0.5vmax  black"
+// })
+// dark.addEventListener("click",function(){
+//     page.style.backgroundColor="black"
+//     light.style.backgroundColor="black"
+//     dark.style.backgroundColor="white"
+//     h1.innerHTML="GOOD NIGHT"
+//     h1.style.left="30%"
+//     h1.style.fontFamily="cursive"
+//     h1.style.color="white"
+//     light.style.fontWeight="900"
+//     dark.style.fontWeight="900"
+//     light.style.color="white"
+//     dark.style.color="black"
+//     dark.style.boxShadow="0 0 1vmax white"
+// })
